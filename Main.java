ba1947fb34f267a4c35dc4b7233c7b715a478bc5
Zadrzavr <83001982+Zadrzavr@users.noreply.github.com>
2021-11-02 16:17:32 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package gd.HomeWork;
+
+class Main {
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+}
